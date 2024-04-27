e6597ede6ef04979066b1f4ecab12cb2533db07f
Ilia Sichkarenko <ilyasichkarenko1999@mail.ru>
2024-04-27 09:57:25 +0300

    Добавлено игральное поле и слот с вегасом

---
+++ Roll-Dices/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Roll-Dices
+//
+//  Created by Илья Сичкаренко on 27.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
